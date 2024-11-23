b847ca001c31b8ce2f789b919baff8a81a363b3f
BikromRoy1 <bikrom@peoplentech.com.bd>
2024-11-23 15:48:18 +0600

    All Startups & Startup Card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,57 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 
-export default async function Home({searchParams}: {searchParams: Promise<{ query?: string }>}) {
-    const query = (await searchParams).query;
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Bikrom" },
+      _id: 1,
+      description:
+        "An intelligent event planning platform that helps users create, organize, and manage events with ease by automating tasks such as venue suggestions, budgeting, guest management, and vendor coordination, all while providing real-time updates and analytics.",
+      image:
+        "https://t4.ftcdn.net/jpg/06/47/14/39/360_F_647143910_ubKhhGWWuV1hy9CgkRJ0lW6MMDQK93Xi.jpg",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
+
   return (
-   <>
-       <section className='pink_container'>
-           <h1 className='heading'>
-               Pitch Your Startup, <br />
-               Connect With Entrepreneurs
-           </h1>
-           <p className='sub-heading !max-w-3xl'>
-               Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
-               Competitions.
-           </p>
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
 
-           <SearchForm query={query} />
-       </section>
-   </>
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
+    </>
   );
 }
